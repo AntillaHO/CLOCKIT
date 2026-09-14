12099088b4e68dbb3ec6e18a7308a7b3f263199e
AntillaHO <antillagraphics@gmail.com>
2026-09-14 09:26:51 +0200

    Add files via upload

--- app/src/androidTest/java/com/example/depthwp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/depthwp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.depthwp", appContext.packageName)
+        assertEquals("com.clockit.studio", appContext.packageName)
     }
 }
